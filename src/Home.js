ba7e3da6d2b8a8899972e9e6f7a16e97e96d36b8
Mark <markkong30@gmail.com>
2021-12-20 23:58:32 +0800

    basic setup

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { json, checkStatus } from "./utils";
+
+class CurrencyApp extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+
+    };
+
+  }
+
+  render () {
+    return (
+      null
+    )
+  }
+}
+
+export default CurrencyApp;
